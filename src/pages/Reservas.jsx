7e5ce6406c425d0dd4337690b5f8de6218477483
Luisa Yepez <luisayepez567@gmail.com>
2026-04-21 08:49:51 -0500

    (chore): implementación de useState para la captura de datos y creación de la función fetch

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 function Reservas() {
-  return (
-    <div>Reservas</div>
-  )
-}
 
-export default Reservas
+
+
+    const Reservas = () => {
+        const [getNombre, setNombre] = useState("")
+        const [getEmail, setEmail] = useState("")
+        const [getExperiencia, setExperiencia] = useState("")
+        const [getMensaje, setMensaje] = useState("")
+        const [getUsers, setUsers] = useState([])
+
+        function fetchUsers() {
+            fetch(end_points.users)
+                .then((response) => response.json())
+                .then((data) => setUsers(data))
+                .catch((error) => console.log(error))
+        }
+
+        useEffect(() => {
+            fetchUsers()
+        }, [])
+
+        return (
+            <div>Reservas</div>
+        )
+    }
+
+    }
+
+    export default Reservas
